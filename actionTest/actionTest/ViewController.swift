c339f765dcfb4927696fc69ca7554d8888688040
Mingyun Cho <chomg88@gmail.com>
2021-11-23 22:56:40 +0900

    init

---
+++ actionTest/actionTest/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  actionTest
+//
+//  Created by mingyun.cho on 2021/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+		
+		let i: Int? = 1
+		
+		print(i)
+	}
+
+
+}
+
